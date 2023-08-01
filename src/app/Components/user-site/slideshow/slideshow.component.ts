0eb0330c2997dc4c3ca21977c8d86f8b4732f9c5
Embact <mrmisho11@gmail.com>
2023-08-01 04:07:37 +0300

    final

--- src/app/Components/user-site/slideshow/slideshow.component.ts
+++ src/app/Components/user-site/slideshow/slideshow.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 interface SlideShow {
   image :string;
+  title:string;
+  description:string;
   url : string;
 }
 
@@ -12,11 +14,10 @@ interface SlideShow {
 })
 export class SlideshowComponent {
   slides : SlideShow[] = [
-    {image:"https://static.vecteezy.com/system/resources/previews/023/905/134/non_2x/christmas-background-with-fir-branches-and-garland-horizontal-banner-with-festive-elements-great-for-ads-flyers-backgrounds-and-more-winter-background-vector.jpg",url:"#"},
-    {image:"https://static.vecteezy.com/system/resources/previews/003/566/555/non_2x/abstract-banner-design-web-templates-horizontal-header-web-banner-modern-abstract-cover-header-background-for-website-design-social-media-cover-advertising-banner-flyer-invitation-card-free-vector.jpg",url:"#"},
-    {image:"https://static.vecteezy.com/system/resources/previews/024/302/482/non_2x/summer-sale-horizontal-banner-with-orange-fruit-cutaway-vector.jpg",url:"#"},
-    {image:"https://static.vecteezy.com/system/resources/previews/003/586/294/non_2x/abstract-banner-design-web-templates-horizontal-header-web-banner-modern-abstract-cover-header-background-for-website-design-social-media-cover-advertising-banner-flyer-invitation-card-free-vector.jpg",url:"#"},
-    {image:"https://previews.123rf.com/images/avgust01/avgust011903/avgust01190300028/124429726-summer-sale-advertisement-banner-horizontal-banner-with-realistic-glass-bottle-with-message.jpg",url:"#"},
-    {image:"https://static.vecteezy.com/system/resources/previews/003/566/561/large_2x/abstract-banner-design-web-templates-horizontal-header-web-banner-modern-abstract-cover-header-background-for-website-design-social-media-cover-advertising-banner-flyer-invitation-card-free-vector.jpg",url:"#"},
+    {image:"../assets/images/slider-2.svg",title:"Welcome to Our Marketplace",description:"Discover amazing deals and unbeatable offers that await you. Shop the best selection at your one-stop shopping destination.",url:"#"},
+    {image:"../assets/images/slider-1.svg",title:"Selling Made Simple",description:"List your items with ease and reach a wide audience of buyers. Experience fast and secure transactions to turn your stuff into cash.",url:"#"},
+    {image:"../assets/images/slider-3.svg",title:"Find Your Perfect Purchase",description:"Explore exclusive products at unbeatable prices and shop with confidence. Discover a world of options as your dream purchase awaits.",url:"#"},
+    {image:"../assets/images/slider-4.svg",title:"Customer Satisfaction Guaranteed",description:"Enjoy personalized customer support and hassle-free returns. Shop with peace of mind, knowing your satisfaction is our priority.",url:"#"}
+
   ]
 }
